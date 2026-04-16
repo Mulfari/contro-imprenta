96b6277f0fbb699098b15f12b7ad97f1af88e67f
Mulfari <119994193+Mulfari@users.noreply.github.com>
2026-04-16 16:43:41 -0400

    copy: update whatsapp prefixed message

--- src/app/storefront-header.tsx
+++ src/app/storefront-header.tsx
@@ -517,7 +517,7 @@ export function StorefrontHeader({
       </section>
 
       <a
-        href="https://wa.me/351922250664?text=Necesito%20ayuda%20con%20mi%20pedido%20en%20Express%20Printer"
+        href="https://wa.me/351922250664?text=Hola%2C%20necesito%20ayuda%20con%20mi%20pedido%20en%20Express%20Printer."
         target="_blank"
         rel="noreferrer"
         aria-label="WhatsApp"
